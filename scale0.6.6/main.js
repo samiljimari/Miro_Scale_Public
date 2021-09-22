4ce019f8006335ab0bba9f2536ec6d368df5e933
Sami Ljimari <48930092+samiljimari@users.noreply.github.com>
2021-09-22 20:57:57 +0200

    Update main.js

--- scale0.6.6/main.js
+++ scale0.6.6/main.js
@@ -1,9 +1,16 @@
-miro.onReady(() => {
+miro.onReady(async () => {
+  const isAuthorized = await miro.isAuthorized()
+
+  if (!isAuthorized) {
+    // Ask the user to authorize the app.
+    await miro.requestAuthorization()
+  }
   miro.initialize({
     extensionPoints: {
-      bottomBar: {
+      toolbar: {
         title: 'Scale',
-        svgIcon: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"> <path d="M21,6.2c-0.6,0-1,0.4-1,1V11h-7V9.3c0-0.6-0.4-1-1-1s-1,0.4-1,1V11H4V7.2c0-0.6-0.4-1-1-1s-1,0.4-1,1v9.6c0,0.6,0.4,1,1,1 s1-0.4,1-1V13h7v1.7c0,0.6,0.4,1,1,1s1-0.4,1-1V13h7v3.8c0,0.6,0.4,1,1,1s1-0.4,1-1V7.2C22,6.6,21.6,6.2,21,6.2z"/> </svg>',
+        librarySvgIcon: '<svg version="1.1" fill="currentColor" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"> <path d="M21,6.2c-0.6,0-1,0.4-1,1V11h-7V9.3c0-0.6-0.4-1-1-1s-1,0.4-1,1V11H4V7.2c0-0.6-0.4-1-1-1s-1,0.4-1,1v9.6c0,0.6,0.4,1,1,1 s1-0.4,1-1V13h7v1.7c0,0.6,0.4,1,1,1s1-0.4,1-1V13h7v3.8c0,0.6,0.4,1,1,1s1-0.4,1-1V7.2C22,6.6,21.6,6.2,21,6.2z"/> </svg>',
+        toolbarSvgIcon: '<svg version="1.1" fill="currentColor" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"> <path d="M21,6.2c-0.6,0-1,0.4-1,1V11h-7V9.3c0-0.6-0.4-1-1-1s-1,0.4-1,1V11H4V7.2c0-0.6-0.4-1-1-1s-1,0.4-1,1v9.6c0,0.6,0.4,1,1,1 s1-0.4,1-1V13h7v1.7c0,0.6,0.4,1,1,1s1-0.4,1-1V13h7v3.8c0,0.6,0.4,1,1,1s1-0.4,1-1V7.2C22,6.6,21.6,6.2,21,6.2z"/> </svg>',
         onClick: function () {
           miro.board.ui.openLeftSidebar('sidebar.html', { width: 400} )
         }
